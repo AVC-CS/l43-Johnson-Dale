794c03342eb74287846135eb228e4d3f54138722
Johnson-Dale <djohnson298@students.avc.edu>
2024-10-11 03:15:55 -0700

    first attempt

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 def main():
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
     total = 0
+    iterator = 0
+
+    while iterator < 5:
+        number = int(input("Input a number: "))
+        total = total + number
+    
+    print(total)
 
     ########################################
     # Do not delete the return statement
